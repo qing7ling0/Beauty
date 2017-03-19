ba7607e7b2a6f682499a6471b2774947bcd8f99f
qing7ling0 <975604172@qq.com>
2017-03-19 22:01:04 +0800

    update

--- app/src/main/java/com/lq/beauty/app/main/widget/RecordButton.java
+++ app/src/main/java/com/lq/beauty/app/main/widget/RecordButton.java
@@ -35,6 +35,9 @@ public class RecordButton extends android.support.v7.widget.AppCompatButton {
     private int mRingColor;
     private int mCircleColor;
 
+    private float mRecordCountDown;
+    private boolean mIsRecording;
+
     public RecordButton(Context context) {
         super(context);
     }
@@ -80,14 +83,28 @@ public class RecordButton extends android.support.v7.widget.AppCompatButton {
 
         mPaint.setColor(mCircleColor);
         canvas.drawCircle(mWidth/2, mHeight/2, mWidth/2, mPaint);
+        if (mIsRecording) {
+            r = r - UIHelper.dp2px(2);
+            mPaint.setColor(mRingColor);
+            canvas.drawCircle(cx, cy, r, mPaint);
 
-        r = r - UIHelper.dp2px(2);
-        mPaint.setColor(mRingColor);
-        canvas.drawCircle(cx, cy, r, mPaint);
+            r = r - UIHelper.dp2px(5);
+            mPaint.setColor(mCircleColor);
+            mPaint.setAlpha((int) (255*(1-mRecordCountDown)));
+            canvas.drawCircle(cx, cy, r, mPaint);
 
-        r = r - UIHelper.dp2px(5);
-        mPaint.setColor(mCircleColor);
-        canvas.drawCircle(cx, cy, r, mPaint);
+            mPaint.setColor(Color.BLACK);
+            mPaint.setAlpha((int) (255*(mRecordCountDown)));
+            canvas.drawCircle(cx, cy, r*mRecordCountDown, mPaint);
+        } else {
+            r = r - UIHelper.dp2px(2);
+            mPaint.setColor(mRingColor);
+            canvas.drawCircle(cx, cy, r, mPaint);
+
+            r = r - UIHelper.dp2px(5);
+            mPaint.setColor(mCircleColor);
+            canvas.drawCircle(cx, cy, r, mPaint);
+        }
     }
 
     /**
@@ -96,25 +113,17 @@ public class RecordButton extends android.support.v7.widget.AppCompatButton {
      * =============================================================================================
      */
     private static final Interpolator ANIMATION_INTERPOLATOR = new DecelerateInterpolator();
-    private static final int ANIMATION_DURATION = 280;
+    private static final int ANIMATION_DURATION = 3000;
     private ObjectAnimator mAnimator;
 
-    @SuppressLint("ObsoleteSdkInt")
-    private void animateCheckedState(boolean newCheckedState) {
+    public void animateCheckedState() {
         AnimatorProperty property = new AnimatorProperty();
-        if (newCheckedState) {
-            property.color = onColor;
-            property.offLineWidth = 10;
-            property.spotX = spotMaxX;
-        } else {
-            property.color = offBorderColor;
-            property.offLineWidth = spotSize;
-            property.spotX = spotMinX;
-        }
-
+        property.progress = 1;
+        mIsRecording = true;
         if (mAnimator == null) {
-            mAnimator = ObjectAnimator.ofObject(this, ANIM_VALUE, new AnimatorEvaluator(mCurProperty), property);
+            mAnimator = ObjectAnimator.ofObject(this, ANIM_VALUE, new AnimatorEvaluator(new AnimatorProperty()), property);
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2)
+
                 mAnimator.setAutoCancel(true);
             mAnimator.setDuration(ANIMATION_DURATION);
             mAnimator.setInterpolator(ANIMATION_INTERPOLATOR);
@@ -134,30 +143,18 @@ public class RecordButton extends android.support.v7.widget.AppCompatButton {
     private AnimatorProperty mCurProperty = new AnimatorProperty();
 
     private void setAnimatorProperty(AnimatorProperty property) {
-        this.progress = property.spotX;
-        this.borderColor = property.color;
-        this.offLineWidth = property.offLineWidth;
+        this.mRecordCountDown = property.progress;
         invalidate();
     }
 
-    private void setAnimatorProperty(boolean isOn) {
+    private void setAnimatorProperty() {
         AnimatorProperty property = mCurProperty;
-        if (isOn) {
-            property.color = onColor;
-            property.offLineWidth = 10;
-            property.spotX = spotMaxX;
-        } else {
-            property.color = offBorderColor;
-            property.offLineWidth = spotSize;
-            property.spotX = spotMinX;
-        }
+        property.progress = mRecordCountDown;
         setAnimatorProperty(property);
     }
 
     private final static class AnimatorProperty {
-        private int color;
-        private float offLineWidth;
-        private float spotX;
+        private float progress;
     }
 
     private final static class AnimatorEvaluator implements TypeEvaluator<AnimatorProperty> {
@@ -170,38 +167,20 @@ public class RecordButton extends android.support.v7.widget.AppCompatButton {
         @Override
         public AnimatorProperty evaluate(float fraction, AnimatorProperty startValue, AnimatorProperty endValue) {
             // Values
-            mProperty.spotX = (int) (startValue.spotX + (endValue.spotX - startValue.spotX) * fraction);
-
-            mProperty.offLineWidth = (int) (startValue.offLineWidth + (endValue.offLineWidth - startValue.offLineWidth) * (1 - fraction));
-
-            // Color
-            int startA = (startValue.color >> 24) & 0xff;
-            int startR = (startValue.color >> 16) & 0xff;
-            int startG = (startValue.color >> 8) & 0xff;
-            int startB = startValue.color & 0xff;
-
-            int endA = (endValue.color >> 24) & 0xff;
-            int endR = (endValue.color >> 16) & 0xff;
-            int endG = (endValue.color >> 8) & 0xff;
-            int endB = endValue.color & 0xff;
-
-            mProperty.color = (startA + (int) (fraction * (endA - startA))) << 24 |
-                    (startR + (int) (fraction * (endR - startR))) << 16 |
-                    (startG + (int) (fraction * (endG - startG))) << 8 |
-                    (startB + (int) (fraction * (endB - startB)));
+            mProperty.progress = startValue.progress + (endValue.progress - startValue.progress) * fraction;
 
             return mProperty;
         }
     }
 
-    private final static Property<ToggleButton, AnimatorProperty> ANIM_VALUE = new Property<ToggleButton, AnimatorProperty>(AnimatorProperty.class, "animValue") {
+    private final static Property<RecordButton, AnimatorProperty> ANIM_VALUE = new Property<RecordButton, AnimatorProperty>(AnimatorProperty.class, "animValue") {
         @Override
-        public AnimatorProperty get(ToggleButton object) {
+        public AnimatorProperty get(RecordButton object) {
             return object.mCurProperty;
         }
 
         @Override
-        public void set(ToggleButton object, AnimatorProperty value) {
+        public void set(RecordButton object, AnimatorProperty value) {
             object.setAnimatorProperty(value);
         }
     };
